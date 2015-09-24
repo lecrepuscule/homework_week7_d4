18bf49211cbaa2e882a8e38e0411d0251ed5e621
Yao Yuan <opal_obsidian@hotmail.com>
2015-09-24 22:38:48 +0100

    index page can show foods using ajax

--- public/js/foods.js
+++ public/js/foods.js
@@ -1,4 +1,25 @@
 $(document).on("ready", function(){
   console.log("testing");
 
+  getIndex();
+
 })
+
+function request(url, method, data){
+  return $.ajax({
+    url: url,
+    method: method,
+    dataType: "json",
+    data: data
+  })
+}
+
+function getIndex(){
+  request("/foods", "get").done(appendFoods);
+}
+
+function appendFoods(foods){
+  $.each(foods, function(index, food){
+    $("<li class='food' data-id=" + food.id + "><a href='#'>" + food.name + "</a></li>").appendTo(".foods");
+  })
+}
